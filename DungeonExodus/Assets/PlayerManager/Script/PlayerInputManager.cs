a12adf35b38fc9aa44a511a29288a7a712bc83d9
DESKTOP-ND36B9N\rabit <Vsteryelim79513!>
2020-12-14 19:39:39 +0900

    add : rotate , add animate, push - block(left click)

--- DungeonExodus/Assets/PlayerManager/Script/PlayerInputManager.cs
+++ DungeonExodus/Assets/PlayerManager/Script/PlayerInputManager.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.VFX;
 
 namespace Assets.PlayerManager.Script
 {
@@ -19,9 +21,15 @@ namespace Assets.PlayerManager.Script
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private float groundCheckRadius = 0.5f;
         [SerializeField] private float _jumpAccel = 0.96f;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private Camera _camera;
+        [SerializeField] private float wheelSpeed = 3000.0f;
+        [SerializeField] private VisualEffect magic;
+        [SerializeField] private GameObject target;
         private void Awake() 
         {
             _characterController = GetComponent<CharacterController>();
+            _animator = GetComponent<Animator>();
         }
         private void OnEnable()
         {
@@ -34,9 +42,25 @@ namespace Assets.PlayerManager.Script
             _playerInput.Player.SetCallbacks(this);
             
         }
-
+        public GameObject Picking()
+        {
+            RaycastHit hit;
+            GameObject target = null;
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray.origin, ray.direction*5f,out hit))
+            {
+                target = hit.collider.gameObject;
+                
+                //hit.transform.GetComponent<MeshRenderer>().material.color = Color.red; // 색 
+                Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+            }
+            return target;
+        
+        }
         void Update()
         {
+            float scroll = Input.GetAxis("Mouse ScrollWheel") * 1000f * Time.deltaTime;
+            //print("wheelSpeed : " + wheelSpeed + "mouseInput" + Input.GetAxis("Mouse ScrollWheel"));
             _isGrounded = Physics.CheckSphere(groundCheckPosition.position, groundCheckRadius, groundLayer );
             
             if (_isGrounded)
@@ -52,7 +76,7 @@ namespace Assets.PlayerManager.Script
                 }
                 _jumpVelocity = _jumpVelocity * _jumpAccel;
                 _fallingSpeed = Time.deltaTime * _jumpVelocity  + Time.deltaTime * gravity;
-                print("jumping");
+               // print("jumping");
             }
             else
             {
@@ -60,13 +84,31 @@ namespace Assets.PlayerManager.Script
             }
             var dir = transform.forward * _inputVector.y + transform.right * _inputVector.x;
             _characterController.Move(dir  * Time.deltaTime * _charactorMoveSpeed);
-
-
-
-                
             _characterController.Move(new Vector3(0, _fallingSpeed, 0));
-            //print(dir.x);    
             
+            
+           var picked = Picking();
+           if (picked == null)
+           {
+     
+           }
+           else if(picked.tag == "mirror")
+           {
+               magic.Play();
+               if (scroll != 0)
+               {
+                   _animator.Play("Attack02Start 0");
+                   picked.transform.Rotate(0, scroll, 0);
+                   print(scroll);
+               }
+           }
+           else if (picked.tag == "block")
+           {
+               magic.Play();
+               var moveDir = transform.forward*Time.deltaTime*5f;
+               moveDir.y = 0;
+               if(Input.GetMouseButton(0)) picked.transform.Translate(moveDir);
+           }
         }
         private void OnDisable()
         {
@@ -75,22 +117,28 @@ namespace Assets.PlayerManager.Script
         public void OnMove(InputAction.CallbackContext context)
         {
             _inputVector = context.ReadValue<Vector2>();
-            print(_inputVector);
+            _animator.Play("BattleRunForward");
         }
-
-        public void OnMouse(InputAction.CallbackContext context)
-        {
-        }
+        
 
         public void OnJump(InputAction.CallbackContext context)
         {
             _isGrounded = false;
             _isJumping = true;
-            print("jump!");
+            _animator.Play("JumpStart");
         }
 
-        public void OnSprint(InputAction.CallbackContext context)
+        public void OnRotate(InputAction.CallbackContext context)
         {
+            target = Picking();
+            var temp = context.ReadValue<Vector2>();
+            if (target.tag == "mirror")
+            {
+                _animator.Play("Attack02Start 0");
+                target.transform.Rotate(0, temp.x * Time.deltaTime * 100f, 0);
+            }
         }
+
+        // print(scroll);
     }
 }
